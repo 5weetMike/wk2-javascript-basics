c78dbe92f71678c7e5c1fdff3f9f56d58cc4e2a2
Michael Hampson <mdavidhampson@gmail.com>
2024-08-05 13:35:50 +0100

    updated after lunch

--- dotnotation.js
+++ dotnotation.js
@@ -1,3 +1,15 @@
 console.log("hello there general kenobi".toUpperCase())
 
 console.log(Math.floor(Math.random()*10))
+
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("---------")
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("---------")
+console.log("  |   |  ")
+console.log("  |   |  ")
+console.log("  |   |  ")
