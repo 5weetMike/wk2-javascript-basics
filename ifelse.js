e4ebb585dc765aeab73573c341e403f4d1a75744
Michael Hampson <mdavidhampson@gmail.com>
2024-08-06 11:11:58 +0100

    updated work

---
+++ ifelse.js
@@ -0,0 +1,33 @@
+// let weather = "Snowy".toLowerCase()
+
+// if (weather == "sunny"){
+//     console.log("well, i will need to wear my shades")
+// } else if (weather == "rainy"){
+//     console.log("I'll wear a raincoat")
+// } else if(weather == "snowy")
+//     {console.log("I'll go snowboarding")
+// }else {
+//     console.log("It could go either way")
+// };
+
+
+// let dicenum = Math.floor(Math.random()*6);
+
+// console.log(`You rolled a ${dicenum}`)
+
+// if (dicenum >= 3) {
+//     console.log("YOU WIN!")
+// } else {
+//     console.log("Try again!")
+// }
+
+let place ="Liverpool";
+let weather= "cloudy";
+
+if(place == "Liverpool" && weather == "sunny"){
+    console.log("check again..")
+} else if (place == "Liverpool" && weather == "rainy"){
+    console.log("obviously")
+} else{
+    console.log ("what its not wet outside!?")
+}
